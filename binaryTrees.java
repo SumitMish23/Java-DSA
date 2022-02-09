c67d67b741c86b1768ea597c8ea27f15c60679a6
SumitMish23 <sumit894sumit@gmail.com>
2022-02-09 20:13:09 +0530

    stacks and queues

---
+++ binaryTrees.java
@@ -0,0 +1,20 @@
+public class binaryTrees {
+
+
+    
+    public class tree{
+        int val;
+        tree left;
+        tree right;
+        tree(int value){
+            this.val=value;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static void main(String args[]) {
+        tree t=new tree(16);
+        
+
+    }
+}
